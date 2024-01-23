2904e360a72a48dbbaf22b82bb15a7934a34102b
Julian Reyes <julianreyes4@gmail.com>
2024-01-23 18:28:27 +0900

    add storybooks.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import storybooks from './storybooks';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <div className='thumbnail'>
         <div className='old-macdonald-thumbnail'><p>Old Macdonald Had A Farm</p>
         </div>
-        <h1>01 - If You're Happy</h1>
+        <h1 className='storybookTitle'>01 - If You're Happy</h1>
         <p>Sprout Series</p>
         <p>Story description here</p>
-        <h1>02 - The ABCs</h1>
+        <h1 className='storybookTitle' >02 - The ABCs</h1>
         <p>Sprout Series</p>
         <p>Story description here</p>
-        <h1>03 - Old Macdonald Had a Farm</h1>
+        <h1 className='storybookTitle' >03 - Old Macdonald Had a Farm</h1>
         <p>Sprout Series</p>
         <p>Story description here</p>
-        <h1>04 - Ten In the Bed</h1>
+        <h1 className='storybookTitle' >04 - Ten In the Bed</h1>
         <p>Sprout Series</p>
         <p>Story description here</p>
-        <h1>05 - The Itsy Bitsy Spider</h1>
+        <h1 className='storybookTitle' >05 - The Itsy Bitsy Spider</h1>
         <p>Sprout Series</p>
         <p>Story description here</p>
-        <h1>06 - The Animals Go Marching</h1>
+        <h1 className='storybookTitle' >06 - The Ants Go Marching</h1>
         <p>Sprout Series</p>
         <p>Story description here</p>
-        <h1>07 - Peanut Butter and Jelly</h1>
+        <h1 className='storybookTitle' >07 - Peanut Butter and Jelly</h1>
         <p>Sprout Series</p>
         <p>Story description here</p>
-        <h1>08 - The Wheels On the Bus</h1>
+        <h1 className='storybookTitle' >08 - The Wheels On the Bus</h1>
         <p>Sprout Series</p>
         <p>Story description here</p>
-        <h1>09 - The Mulberry Bush</h1>
+        <h1 className='storybookTitle' >09 - The Mulberry Bush</h1>
         <p>Sprout Series</p>
         <p>Story description here</p>
-        <h1>10 - The Green Grass</h1>
+        <h1 className='storybookTitle' >10 - The Green Grass</h1>
         <p>Sprout Series</p>
         <p>Story description here</p>
       </div>
---
+++ src/storybooks.js
@@ -0,0 +1,84 @@
+const storybooks = [
+    {
+        id: 1,
+        title: "If You're Happy",
+        description: "If You’re Happy teaches us about the different ways we can express ourselves.",
+        coverImage: "../public/old-macdonald-had-a-farm-image-1.jpg",
+        series: "Sprout",
+        numOfPages: 25
+    },
+    {
+        id: 2,
+        title: "The ABCs",
+        description: "The ABCs introduces the alphabet to new and young leaners.",
+        coverImage: "../public/old-macdonald-had-a-farm-image-1.jpg",
+        series: "Sprout",
+        numOfPages: 17
+    },
+    {
+        id: 3,
+        title: "Old MacDonald Had a Farm",
+        description: "Old MacDonald Had a Farm tells about a farm with many animals on it and all the different sounds that they make.",
+        coverImage: "../public/old-macdonald-had-a-farm-image-1.jpg",
+        series: "Sprout",
+        numOfPages: 21
+    },
+    {
+        id: 4,
+        title: "Ten In the Bed",
+        description: "The ABCs introduces the alphabet to new and young leaners.",
+        coverImage: "../public/old-macdonald-had-a-farm-image-1.jpg",
+        series: "Sprout",
+        numOfPages: 39
+    },
+    {
+        id: 5,
+        title: "The Itsy Bitsy Spider",
+        description: "The ABCs introduces the alphabet to new and young leaners.",
+        coverImage: "../public/old-macdonald-had-a-farm-image-1.jpg",
+        series: "Sprout",
+        numOfPages: 26
+    },
+    {
+        id: 6,
+        title: "The Ants Go Marching",
+        description: "If You’re Happy teaches us about the different ways we can express ourselves.",
+        coverImage: "../public/old-macdonald-had-a-farm-image-1.jpg",
+        series: "Sprout",
+        numOfPages: 25
+    },
+    {
+        id: 7,
+        title: "Peanut Butter and Jelly",
+        description: "The ABCs introduces the alphabet to new and young leaners.",
+        coverImage: "../public/old-macdonald-had-a-farm-image-1.jpg",
+        series: "Sprout",
+        numOfPages: 17
+    },
+    {
+        id: 8,
+        title: "The Wheels On the Bus",
+        description: "The Wheels on the Bus explains how many different things happen on a bus all day long.",
+        coverImage: "../public/old-macdonald-had-a-farm-image-1.jpg",
+        series: "Sprout",
+        numOfPages: 13
+    },
+    {
+        id: 9,
+        title: "The Mulberry Bush",
+        description: "The Mulberry Bush introduces us to the different parts of the body.",
+        coverImage: "../public/old-macdonald-had-a-farm-image-1.jpg",
+        series: "Sprout",
+        numOfPages: 19
+    },
+    {
+        id: 10,
+        title: "The Green Grass",
+        description: "The ABCs introduces the alphabet to new and young leaners.",
+        coverImage: "../public/old-macdonald-had-a-farm-image-1.jpg",
+        series: "Sprout",
+        numOfPages: 17
+    }
+]
+
+export default storybooks;
