b388c76e8fae41dc7c2f8ba481f947a25422db70
Julian Reyes <julianreyes4@gmail.com>
2024-02-16 13:14:04 +0900

    update new 2

--- src/components/StorybookCard/StorybookCard.jsx
+++ src/components/StorybookCard/StorybookCard.jsx
@@ -3,6 +3,8 @@ import styles from "./StorybookCard.module.css"
 
 export default function StorybookCard() {
     return (
-        <h1>this is the storybook card.</h1>
+        <div className={styles.backgroundTest} >
+            <h1>this is the storybook card.</h1>
+        </div>
     )
 }
